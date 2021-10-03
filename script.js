4da230a06d3a2aea5b54220bfe774190853383eb
Kevin McPeak <kevmc46@gmail.com>
2021-10-03 14:58:02 -0400

    form inputs pushes to webpage

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let myLibrary = [];
 let button = document.querySelector("#button");
 let books =document.querySelector("#books");
+let libTitle = document.querySelector("#libTitle")
+let libAuthor = document.querySelector("#libAuthor")
+let libPages = document.querySelector("#libPages")
+let libRead = document.querySelector("#libRead")
 
 
 function Book(title, author, pages, read){
@@ -11,7 +15,7 @@ function Book(title, author, pages, read){
 };
 
 function addBookToLibrary(book){
-    return myLibrary.push(book);
+     myLibrary.push(book);
 };
 
 function submitButton(){
@@ -21,20 +25,46 @@ function submitButton(){
     let read = document.querySelector("#read").value;
 
     //create book from input fields:
-    let newBook = new Book(title,author,pages,read)
-
+    let newBook = new Book(title,author,pages,read);
+    
     //push book to 'myLibary'
     addBookToLibrary(newBook);  
-    
-    for (let i = 0; i < myLibrary.length; i++){
-        let newDiv = document.createElement("DIV");
-        newDiv.innerHTML = "hello";
-        books.appendChild(newDiv);
-    };
 
-    console.log(myLibrary);
+    toPage(myLibrary);
+  
+};
+
+
+function toPage(arr){
+    for (let title in myLibrary){
+        let newTitle = document.createElement('DIV');
+        newTitle.textContent = (`${myLibrary[title].title}`);
+        libTitle.appendChild(newTitle);
+    }
+    
+    for (let author in myLibrary){
+        let newAuthor = document.createElement('DIV');
+        newAuthor.textContent = (`${myLibrary[author].author}`);
+        libAuthor.appendChild(newAuthor);
+    }
+    
+    for (let pages in myLibrary){
+        let newPages = document.createElement('DIV');
+        newPages.textContent = (`${myLibrary[pages].pages}`);
+        libPages.appendChild(newPages);
+    }
+    for (let read in myLibrary){
+        let newRead = document.createElement('DIV');
+        newRead.textContent = (`${myLibrary[read].read}`);
+        libRead.appendChild(newRead);
+    }
 }
 
-
-//Event Listener
 button.addEventListener("click", submitButton);
+
+
+
+
+
+
+
